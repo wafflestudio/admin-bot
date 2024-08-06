7eec6b8b1ab14bd90a20648f9ee28a084e2e7947
plgafhd <plgafhd@snu.ac.kr>
2024-08-06 17:52:58 +0900

    어느 정도 성공?

--- kanban-reporter/src/App.java
+++ kanban-reporter/src/App.java
@@ -1,6 +1,33 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        System.out.println("Hello, World!");
+        // Read Environment, Default = dev
+        String environment = System.getProperty("env", "dev");
+
+        // Set path for config.properties
+        String configFileName = "../config/" + environment + "/config.properties";
+
+        // Load Properties
+        Properties properties = new Properties();
+
+        try (InputStream input = new FileInputStream(configFileName)) {
+            // Load properties file
+            properties.load(input);
+
+            // Get properties
+            String notionDatabaseId = properties.getProperty("NOTION_DATABASE_ID", null);
+            String notionToken = properties.getProperty("NOTION_TOKEN", null);
+
+            // Print properties
+            System.out.println("Notion Database ID: " + notionDatabaseId);
+            System.out.println("Notion Token: " + notionToken);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
