09d4bfdbcaaeeac44406132a37ef53dff05dc651
plgafhd <plgafhd@snu.ac.kr>
2024-08-06 17:30:44 +0900

    dotenv 삭제

--- kanban-reporter/src/App.java
+++ kanban-reporter/src/App.java
@@ -1,8 +1,5 @@
-import io.github.cdimascio.dotenv.Dotenv;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        Dotenv dotenv = Dotenv.load();
         
         System.out.println("Hello, World!");
     }
