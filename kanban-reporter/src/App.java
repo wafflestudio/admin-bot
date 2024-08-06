8b10de5634a604f64d44a7be1a9dfba69fc0a29e
plgafhd <plgafhd@snu.ac.kr>
2024-08-06 15:11:45 +0900

    dotent-java 설치

--- kanban-reporter/src/App.java
+++ kanban-reporter/src/App.java
@@ -1,5 +1,9 @@
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        Dotenv dotenv = Dotenv.load();
+        
         System.out.println("Hello, World!");
     }
 }
