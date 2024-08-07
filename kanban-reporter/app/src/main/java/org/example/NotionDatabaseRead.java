0e50746444f6dd4afced183a0c338158fa0caea9
plgafhd <plgafhd@snu.ac.kr>
2024-08-07 15:46:26 +0900

    노션 읽어오는데 성공!

--- kanban-reporter/app/src/main/java/org/example/NotionDatabaseRead.java
+++ kanban-reporter/app/src/main/java/org/example/NotionDatabaseRead.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class NotionDatabaseRead {
     private final String NOTION_TOKEN;
@@ -21,12 +24,25 @@ public class NotionDatabaseRead {
 
         try {
             OkHttpClient client = new OkHttpClient();
+            
+            MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+            String jsonString = "{}";
+
+            RequestBody body = RequestBody.create(jsonString, JSON);
 
             Request request = new Request.Builder()
-                .url("https://api.notion.com/v1/databases/" + DATABASE_ID)
+                .url("https://api.notion.com/v1/databases/" + DATABASE_ID + "/query")
+                .addHeader("Authorization", "Bearer " + NOTION_TOKEN)
+                .addHeader("Notion-Version", "2022-06-28")
+                .addHeader("Content-Type", "application/json")
+                .post(body)
                 .build();
+
+            Response response = client.newCall(request).execute();
+
+            System.out.println(response.code());
         } catch (Exception e) {
-            // TODO: handle exception
+            System.out.println(e);
         }
     }
 }
